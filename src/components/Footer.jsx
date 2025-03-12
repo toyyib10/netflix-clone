a02f545199d5bb18472b041aa9154965e6961fd8
toyyib10 <yekeentoyyib@gmail.com>
2025-03-12 15:30:08 +0100

    started working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='bg-black'>
+        <a href="" className='text-gray-400 '>Questions? Contact us.</a>
+        <div className="">
+            <ul className="">
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+            </ul>
+            <ul className="">
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+            </ul>
+            <ul className="">
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+            </ul>
+            <ul className="">
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+                <li><a href="" className=""></a></li>
+            </ul>
+        </div>
+        <select name="" id=""></select>
+        <p></p>
     </div>
   )
 }
